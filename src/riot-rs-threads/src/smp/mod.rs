3b8aed5ee5d376e6690770d25badf955c4a544a6
Elena Frank <elena.frank@protonmail.com>
2024-04-09 15:50:48 +0200

    threads: PoC multicore support for rp2040

---
+++ src/riot-rs-threads/src/smp/mod.rs
@@ -0,0 +1,28 @@
+use super::CoreId;
+
+pub trait Multicore {
+    const CORES: u32;
+
+    fn core_id() -> CoreId;
+
+    fn startup_cores();
+}
+
+cfg_if::cfg_if! {
+    if #[cfg(context = "rp2040")] {
+        mod rp2040;
+        pub use rp2040::Chip;
+    }
+    else {
+        pub struct Chip;
+        impl Multicore for Chip {
+            const CORES: u32 = 1;
+
+            fn core_id() -> CoreId {
+                CoreId::new(0)
+            }
+
+            fn startup_cores() { }
+        }
+    }
+}
