0b5aa909dee21304cee6cc401797b3840dae1e0b
Elena Frank <elena.frank@protonmail.com>
2024-05-02 14:47:19 +0200

    refactor(threads): don't modify runqueue in `Threads::set_state`
    - It's not intuitive that setting the state also updates the runqueue.
    - On the caller side we already have the info about the previous and
      next state (that is needed to determine if the runqueue should be
    modified) so we can safe some checks.

--- src/riot-rs-threads/src/arch/cortex_m.rs
+++ src/riot-rs-threads/src/arch/cortex_m.rs
@@ -1,5 +1,4 @@
 use super::Arch;
-use crate::smp::Multicore;
 use crate::Thread;
 use core::arch::asm;
 use core::ptr::write_volatile;
@@ -184,7 +183,6 @@ unsafe extern "C" fn PendSV() {
 // TODO: make arch independent, or move to arch
 #[no_mangle]
 unsafe fn sched() -> u128 {
-    let core_id = crate::smp::Chip::core_id() as usize;
     loop {
         if let Some(res) = critical_section::with(|cs| {
             let threads = unsafe { &mut *THREADS.as_ptr(cs) };
@@ -218,7 +216,7 @@ unsafe fn sched() -> u128 {
                 current_high_regs = current.data.as_ptr();
             }
 
-            threads.current_threads[core_id] = Some(next_pid);
+            *threads.current_pid_mut() = Some(next_pid);
 
             let next = &threads.threads[usize::from(next_pid)];
             let next_sp = next.sp;
