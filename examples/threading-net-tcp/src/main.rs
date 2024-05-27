69c69ad26c7ead96ba8921d78a46cee79c96afc2
Elena Frank <elena.frank@protonmail.com>
2024-05-27 10:57:38 +0200

    embassy: create esp-wifi-thread if threading enabled

--- examples/threading-net-tcp/src/main.rs
+++ examples/threading-net-tcp/src/main.rs
@@ -4,71 +4,12 @@
 #![feature(used_with_arg)]
 
 use embassy_net::tcp::TcpSocket;
+use embassy_time::Duration;
 use embedded_io_async::Write;
-use esp_hal::{
-    interrupt,
-    peripherals::{Interrupt, SYSTIMER},
-};
-use riot_rs::{
-    debug::println,
-    embassy::network,
-    thread::{thread_flags, ThreadId},
-};
+use riot_rs::{debug::println, embassy::network};
 
-#[cfg(context = "esp32c6")]
-use esp_hal::peripherals::INTPRI as SystemPeripheral;
-#[cfg(context = "esp32c3")]
-use esp_hal::peripherals::SYSTEM as SystemPeripheral;
-
-// Handle the systimer alarm 0 interrupt, configured in esp-wifi.
-extern "C" fn systimer_target0_() {
-    unsafe {
-        SYSTIMER::steal()
-            .int_clr()
-            .write(|w| w.target0().clear_bit_by_one())
-    }
-    // Wakeup `esp_wifi_thread`.
-    riot_rs::thread::wakeup(ThreadId::new(0));
-}
-
-// CPU Interrupt 3 triggers the scheduler in `esp-wifi`.
-fn yield_to_esp_wifi_scheduler() {
-    unsafe {
-        (&*SystemPeripheral::PTR)
-            .cpu_intr_from_cpu_3()
-            .modify(|_, w| w.cpu_intr_from_cpu_3().set_bit());
-    }
-}
-
-/// High priority thread that frequently wakes up to run the esp-wifi
-/// scheduler.
-#[riot_rs::thread(autostart, priority = 10, stacksize = 4096)]
-fn esp_wifi_thread() {
-    // Wait until `embassy` was intialized.
-    thread_flags::wait_all(1);
-
-    // Bind the periodic systimer that is configured in esp-wifi to our own handler.
-    unsafe {
-        interrupt::bind_interrupt(
-            Interrupt::SYSTIMER_TARGET0,
-            core::mem::transmute(systimer_target0_ as *const ()),
-        );
-    }
-
-    loop {
-        // Yield to the esp-wifi scheduler tasks, so that they get a chance to run.
-        yield_to_esp_wifi_scheduler();
-        // Sleep until the systimer alarm 0 interrupts again.
-        riot_rs::thread::sleep()
-    }
-}
-
-/// Application task.
 #[riot_rs::task(autostart)]
 async fn tcp_echo() {
-    // Start the esp-wifi thread.
-    thread_flags::set(ThreadId::new(0), 1);
-
     let stack = network::network_stack().await.unwrap();
 
     let mut rx_buffer = [0; 4096];
@@ -77,7 +18,7 @@ async fn tcp_echo() {
 
     loop {
         let mut socket = TcpSocket::new(stack, &mut rx_buffer, &mut tx_buffer);
-        socket.set_timeout(Some(embassy_time::Duration::from_secs(10)));
+        socket.set_timeout(Some(Duration::from_secs(10)));
 
         println!("Listening on TCP:1234...");
         if let Err(e) = socket.accept(1234).await {
@@ -99,6 +40,8 @@ async fn tcp_echo() {
                     break;
                 }
             };
+
+            //println!("rxd {:02x}", &buf[..n]);
 
             match socket.write_all(&buf[..n]).await {
                 Ok(()) => {}
