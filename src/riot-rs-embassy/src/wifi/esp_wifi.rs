ddc09757a73e7a75f6a99936d2bed8b4298c20f9
Elena Frank <elena.frank@protonmail.com>
2024-08-07 11:59:38 +0200

    fix(embassy/esp-wifi) always delegate to wifi thread
    We should only ever hand off to the esp-wifi scheduler if we are inside
    the `esp_wifi_thread`.
    If this isn't the case (e.g. esp-wifi also triggers its scheduler from inside
    an ISR), the esp-wifi thread should first be woken up so that it can do
    the hand-off.

--- src/riot-rs-embassy/src/wifi/esp_wifi.rs
+++ src/riot-rs-embassy/src/wifi/esp_wifi.rs
@@ -38,11 +38,11 @@ async fn connection(mut controller: WifiController<'static>) {
     #[cfg(feature = "threading")]
     {
         let thread_id = WIFI_THREAD_ID.get().unwrap();
-        riot_rs_threads::wakeup(*thread_id);
+        riot_rs_threads::thread_flags::set(*thread_id, 0b1);
     }
 
     debug!("start connection task");
-    debug!("Device capabilities: {:?}", controller.get_capabilities());
+    debug!("Device configuration: {:?}", controller.get_configuration());
     loop {
         match esp_wifi::wifi::get_wifi_state() {
             WifiState::StaConnected => {
@@ -88,58 +88,92 @@ mod wifi_thread {
         peripherals::{Interrupt, SYSTIMER},
     };
 
-    // Handle the systimer alarm 0 interrupt, configured in esp-wifi.
-    extern "C" fn systimer_target0_() {
+    extern "Rust" {
+        // `esp_wifi` handler that does the context switching.
+        fn FROM_CPU_INTR3(trap_frame: &mut interrupt::TrapFrame);
+    }
+
+
+    /// Interceptor for yielding to the esp-wifi scheduler.
+    ///
+    /// It only hands over to the esp-wifi scheduler if we're in the context 
+    /// of the `esp_wifi_thread`. 
+    /// Otherwise it first initiates a context switch to `esp_wifi_thread`, which
+    /// will then trigger again a task yield there.
+    fn intercept_task_yield(trap_frame: &mut interrupt::TrapFrame) {
+        clear_interrupts();
+
+        let wifi_thread_pid = WIFI_THREAD_ID.get().unwrap();
+
+        // Check if we are currently in the context of the `esp_wifi_thread`.
+        // If that's the case, hand over to the esp-wifi scheduler so that it can run its task.
+        // Because `esp_wifi_thread` runs at highest priority, it won't be preempted.
+        // If not, wake up `esp_wifi_thread`.
+        match riot_rs_threads::current_pid() {
+            Some(pid) if pid == *wifi_thread_pid && riot_rs_threads::is_running(pid) => unsafe {
+                FROM_CPU_INTR3(trap_frame)
+            },
+            _ => {
+                riot_rs_threads::wakeup(*wifi_thread_pid);
+            }
+        }
+    }
+
+    /// Thread that runs the esp-wifi scheduler.
+    /// 
+    /// Because it runs at highest priority, it can't be preempted by any riot-rs threads and therefore
+    /// the two schedulers won't interleave.
+    #[riot_rs_macros::thread(autostart, stacksize = 4096, priority = riot_rs_threads::SCHED_PRIO_LEVELS as u8 - 1)]
+    fn esp_wifi_thread() {
+        WIFI_THREAD_ID
+            .set(riot_rs_threads::current_pid().unwrap())
+            .unwrap();
+
+        // Wait until `embassy` was initialized.
+        riot_rs_threads::thread_flags::wait_one(0b1);
+
+        // Intercept the esp-wifi interrupts with our own handler.
+        for intr in [Interrupt::SYSTIMER_TARGET0, Interrupt::FROM_CPU_INTR3] {
+            unsafe {
+                interrupt::bind_interrupt(
+                    intr,
+                    core::mem::transmute(intercept_task_yield as *const ()),
+                );
+            }
+            interrupt::enable(intr, interrupt::Priority::Priority2).unwrap();
+        }
+
+        loop {
+            // Thread will be woken up by `intercept_task_yield` when an esp-wifi 
+            // task or ISR attempts to yield to the esp-wifi scheduler.
+            riot_rs_threads::sleep();
+            // Yield again now that we are in the correct context.
+            yield_task();
+        }
+    }
+
+    fn clear_interrupts() {
         unsafe {
+            // Clear CPU Interrupt 3.
+            (*SystemPeripheral::PTR)
+                .cpu_intr_from_cpu_3()
+                .modify(|_, w| w.cpu_intr_from_cpu_3().clear_bit());
+
+            // Clear systimer target 0 interrupt.
             SYSTIMER::steal()
                 .int_clr()
                 .write(|w| w.target0().clear_bit_by_one())
         }
-        // Wakeup `esp_wifi_thread`.
-        riot_rs_threads::wakeup(*WIFI_THREAD_ID.get().unwrap());
     }
 
-    // CPU Interrupt 3 triggers the scheduler in `esp-wifi`.
-    fn yield_to_esp_wifi_scheduler() {
+    fn yield_task() {
         unsafe {
-            (&*SystemPeripheral::PTR)
-                .cpu_intr_from_cpu_3()
-                .modify(|_, w| w.cpu_intr_from_cpu_3().set_bit());
+            let cpu_intr_3 = (&*SystemPeripheral::PTR).cpu_intr_from_cpu_3();
+
+            cpu_intr_3.modify(|_, w| w.cpu_intr_from_cpu_3().set_bit());
+            // Avoid that any subsequent code is executed before the interrupt
+            // actually triggered.
+            while cpu_intr_3.read().cpu_intr_from_cpu_3().bit() {}
         }
-    }
-
-    /// High priority thread that frequently wakes up to run the esp-wifi
-    /// scheduler.
-    fn esp_wifi_thread() {
-        // Wait until `embassy` was intialized.
-        riot_rs_threads::sleep();
-
-        // Bind the periodic systimer that is configured in esp-wifi to our own handler.
-        unsafe {
-            interrupt::bind_interrupt(
-                Interrupt::SYSTIMER_TARGET0,
-                core::mem::transmute(systimer_target0_ as *const ()),
-            );
-        }
-
-        loop {
-            interrupt::disable(esp_hal::Cpu::ProCpu, Interrupt::SYSTIMER_TARGET0);
-            // Yield to the esp-wifi scheduler tasks, so that they get a chance to run.
-            yield_to_esp_wifi_scheduler();
-            interrupt::enable(Interrupt::SYSTIMER_TARGET0, interrupt::Priority::Priority2).unwrap();
-            // Sleep until the systimer alarm 0 interrupts again.
-            riot_rs_threads::sleep()
-        }
-    }
-
-    #[linkme::distributed_slice(riot_rs_threads::THREAD_FNS)]
-    fn start_esp_wifi_thread() {
-        let stack = static_cell::make_static!([0u8; 2048]);
-        let id = riot_rs_threads::thread_create_noarg(
-            esp_wifi_thread,
-            stack,
-            riot_rs_threads::SCHED_PRIO_LEVELS as u8 - 1,
-        );
-        WIFI_THREAD_ID.set(id).unwrap();
     }
 }
