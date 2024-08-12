210b9d5982d96b64e53056ccc3ed7e3da3c00b60
Elena Frank <elena.frank@protonmail.com>
2024-08-12 17:10:23 +0200

    feat(threads): trigger scheduler on other core
    Add `Multicore::schedule_on_core`, which uses the FIFO queues between
    the cores to trigger the scheduler on the other core.

--- src/riot-rs-threads/src/smp/mod.rs
+++ src/riot-rs-threads/src/smp/mod.rs
@@ -24,6 +24,8 @@ pub trait Multicore {
     fn wait_for_wakeup();
 
     fn sev();
+
+    fn schedule_on_core(id: CoreId);
 }
 
 cfg_if::cfg_if! {
@@ -49,10 +51,16 @@ cfg_if::cfg_if! {
             }
 
             fn sev() {}
+
+            fn schedule_on_core(_id: CoreId) {}
         }
     }
 }
 
 pub fn sev() {
     Chip::sev()
+}
+
+pub fn schedule_on_core(id: CoreId) {
+    Chip::schedule_on_core(id)
 }
