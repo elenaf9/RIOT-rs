dae6e0584c632a772a357a0ceb8770e3fd103899
Elena Frank <elena.frank@protonmail.com>
2024-09-05 16:33:35 +0200

    feat(threads): add mutex with prio inheritance

--- src/riot-rs-threads/src/threadlist.rs
+++ src/riot-rs-threads/src/threadlist.rs
@@ -1,12 +1,10 @@
-use critical_section::CriticalSection;
-
-use crate::{thread::Thread, ThreadId, ThreadState, THREADS};
+use crate::{thread::Thread, RunqueueId, ThreadId, ThreadState, Threads};
 
 /// Manages blocked [`super::Thread`]s for a resource, and triggering the scheduler when needed.
 #[derive(Debug, Default)]
 pub struct ThreadList {
     /// Next thread to run once the resource is available.
-    pub head: Option<ThreadId>,
+    head: Option<ThreadId>,
 }
 
 impl ThreadList {
@@ -17,30 +15,37 @@ impl ThreadList {
 
     /// Puts the current (blocked) thread into this [`ThreadList`] and triggers the scheduler.
     ///
+    /// Returns a `RunqueueId` if the highest priority among the waiters in the list has changed.
+    ///
     /// # Panics
     ///
     /// Panics if this is called outside of a thread context.
-    pub fn put_current(&mut self, cs: CriticalSection, state: ThreadState) {
-        THREADS.with_mut_cs(cs, |mut threads| {
-            let &mut Thread { pid, prio, .. } = threads
-                .current()
-                .expect("Function should be called inside a thread context.");
-            let mut curr = None;
-            let mut next = self.head;
-            while let Some(n) = next {
-                if threads.get_unchecked_mut(n).prio < prio {
-                    break;
-                }
-                curr = next;
-                next = threads.thread_blocklist[usize::from(n)];
+    pub fn put_current(&mut self, threads: &mut Threads, state: ThreadState) -> Option<RunqueueId> {
+        let &mut Thread { pid, prio, .. } = threads
+            .current()
+            .expect("Function should be called inside a thread context.");
+        let mut curr = None;
+        let mut next = self.head;
+        while let Some(n) = next {
+            if threads.get_unchecked_mut(n).prio < prio {
+                break;
             }
-            threads.thread_blocklist[usize::from(pid)] = next;
-            match curr {
-                Some(curr) => threads.thread_blocklist[usize::from(curr)] = Some(pid),
-                _ => self.head = Some(pid),
+            curr = next;
+            next = threads.thread_blocklist[usize::from(n)];
+        }
+        threads.thread_blocklist[usize::from(pid)] = next;
+        let inherit_priority = match curr {
+            Some(curr) => {
+                threads.thread_blocklist[usize::from(curr)] = Some(pid);
+                None
             }
-            threads.set_state(pid, state);
-        });
+            _ => {
+                self.head = Some(pid);
+                Some(prio)
+            }
+        };
+        threads.set_state(pid, state);
+        inherit_priority
     }
 
     /// Removes the head from this [`ThreadList`].
@@ -49,17 +54,15 @@ impl ThreadList {
     /// the scheduler.
     ///
     /// Returns the thread's [`ThreadId`] and its previous [`ThreadState`].
-    pub fn pop(&mut self, cs: CriticalSection) -> Option<(ThreadId, ThreadState)> {
+    pub fn pop(&mut self, threads: &mut Threads) -> Option<(ThreadId, ThreadState)> {
         let head = self.head?;
-        THREADS.with_mut_cs(cs, |mut threads| {
-            self.head = threads.thread_blocklist[usize::from(head)].take();
-            let old_state = threads.set_state(head, ThreadState::Running);
-            Some((head, old_state))
-        })
+        self.head = threads.thread_blocklist[usize::from(head)].take();
+        let old_state = threads.set_state(head, ThreadState::Running);
+        Some((head, old_state))
     }
 
     /// Determines if this [`ThreadList`] is empty.
-    pub fn is_empty(&self, _cs: CriticalSection) -> bool {
+    pub fn is_empty(&self) -> bool {
         self.head.is_none()
     }
 }
