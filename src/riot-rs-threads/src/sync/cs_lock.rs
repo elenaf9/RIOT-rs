6c68f6ecf123f0d1e5a67da262e43d27a2cfd570
Elena Frank <elena.frank@protonmail.com>
2024-09-28 18:32:16 +0200

    feat(threads): add `Cslock`

---
+++ src/riot-rs-threads/src/sync/cs_lock.rs
@@ -0,0 +1,54 @@
+//! This module provides a Spinlock implementation.
+use core::cell::UnsafeCell;
+
+use critical_section::CriticalSection;
+
+use crate::smp::{Chip, Multicore};
+
+/// A basic binary spinlock.
+///
+/// A `Spinlock` behaves like a Mutex, but carries no data.
+/// This is supposed to be used to implement other locking primitives.
+pub struct CsLock<T> {
+    inner: UnsafeCell<T>,
+}
+
+impl<T> CsLock<T> {
+    pub const fn new(inner: T) -> Self {
+        Self {
+            inner: UnsafeCell::new(inner),
+        }
+    }
+
+    pub fn with<F, R>(&self, tkn: &mut <Chip as Multicore>::NoPreemptionToken<'_>, f: F) -> R
+    where
+        F: FnOnce(&T) -> R,
+    {
+        Chip::multicore_lock_with(tkn, |cs| self.with_cs(cs, f))
+    }
+
+    pub fn with_cs<F, R>(&self, _cs: CriticalSection, f: F) -> R
+    where
+        F: FnOnce(&T) -> R,
+    {
+        let inner = unsafe { &*self.inner.get() };
+        f(inner)
+    }
+
+    pub fn with_mut<F, R>(&self, tkn: &mut <Chip as Multicore>::NoPreemptionToken<'_>, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R,
+    {
+        Chip::multicore_lock_with(tkn, |cs| self.with_mut_cs(cs, f))
+    }
+
+    pub fn with_mut_cs<F, R>(&self, _cs: CriticalSection, f: F) -> R
+    where
+        F: FnOnce(&mut T) -> R,
+    {
+        let inner = unsafe { &mut *self.inner.get() };
+        f(inner)
+    }
+}
+
+unsafe impl<T> Sync for CsLock<T> {}
