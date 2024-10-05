a0870dbd323c48b31011fbd73a05c09366b2d6a4
Elena Frank <elena.frank@protonmail.com>
2024-10-05 16:37:07 +0200

    feat(threads): add `AtomicLock`

--- src/riot-rs-threads/src/sync/mod.rs
+++ src/riot-rs-threads/src/sync/mod.rs
@@ -1,8 +1,10 @@
 //! Synchronization primitives.
+mod atomic_lock;
 mod channel;
 mod lock;
 mod mutex;
 
+pub use atomic_lock::AtomicLock;
 pub use channel::Channel;
 pub use lock::Lock;
 pub use mutex::{Mutex, MutexGuard};
