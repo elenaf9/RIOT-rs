919170bc229d42ecbbff278b1a8c6045936dd954
Elena Frank <elena.frank@protonmail.com>
2024-10-06 20:04:25 +0200

    fix(threads): prevent deadlocks

--- src/riot-rs-threads/src/sync/lock.rs
+++ src/riot-rs-threads/src/sync/lock.rs
@@ -54,12 +54,12 @@ impl Lock {
     /// Panics if this is called outside of a thread context.
     pub fn acquire(&self) {
         critical_section::with(|cs| {
-            THREADS.with_cs(cs, |threads| {
+            THREADS.with_cs(cs, |mut threads| {
                 let state = unsafe { &mut *self.state.get() };
                 match *state {
                     LockState::Unlocked => *state = LockState::Locked(ThreadList::new()),
                     LockState::Locked(ref mut waiters) => {
-                        waiters.put_current(&threads, ThreadState::LockBlocked);
+                        waiters.put_current(&mut threads, ThreadState::LockBlocked);
                     }
                 }
             })
@@ -91,12 +91,12 @@ impl Lock {
     /// If the lock was not locked, the function just returns.
     pub fn release(&self) {
         critical_section::with(|cs| {
-            THREADS.with_cs(cs, |threads| {
+            THREADS.with_cs(cs, |mut threads| {
                 let state = unsafe { &mut *self.state.get() };
                 match *state {
                     LockState::Unlocked => {}
                     LockState::Locked(ref mut waiters) => {
-                        if waiters.pop(&threads).is_none() {
+                        if waiters.pop(&mut threads).is_none() {
                             *state = LockState::Unlocked
                         }
                     }
