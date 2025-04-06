538f0602ac528fb94f792afecd6facdd252d1d64
Elena Frank <elena.frank@protonmail.com>
2025-04-06 22:26:58 +1200

    fix(examples): don't use core affinities

--- examples/threading-multicore/src/main.rs
+++ examples/threading-multicore/src/main.rs
@@ -2,16 +2,13 @@
 #![no_std]
 #![feature(type_alias_impl_trait)]
 
-use ariel_os::{
-    debug::log::*,
-    thread::{CoreAffinity, CoreId},
-};
+use ariel_os::debug::log::*;
 
-#[ariel_os::thread(autostart, affinity = CoreAffinity::one(CoreId::new(1)), priority = 2)]
+#[ariel_os::thread(autostart, priority = 2)]
 fn thread0() {
     let core = ariel_os::thread::core_id();
     let tid = ariel_os::thread::current_tid().unwrap();
-    info!("Hello from {:?} on {:?}, I am pinned to core 1", tid, core);
+    info!("Hello from {:?} on {:?}", tid, core);
     loop {}
 }
 
